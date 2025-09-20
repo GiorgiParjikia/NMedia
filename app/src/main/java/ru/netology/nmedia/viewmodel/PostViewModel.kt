45dfe141c2a9fb5db0c984c4f1940dd7a2f1eaf0
Giorgi Parjikia <parjikia5@gmail.com>
2025-09-20 20:20:40 +0400

    feat: add PostRepositoryFileImpl with JSON persistence

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,11 +1,15 @@
 package ru.netology.nmedia.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryFileImpl
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import ru.netology.nmedia.repository.PostRepositorySharedPrefImpl
 
 private val empty = Post(
 id = 0,
@@ -15,9 +19,11 @@ content = "",
 likeByMe = false
 )
 
-class PostViewModel : ViewModel() {
+class PostViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+    // private val repository: PostRepository = PostRepositoryInMemoryImpl()
+    // private val repository: PostRepository = PostRepositorySharedPrefImpl(application)
+    private val repository: PostRepository = PostRepositoryFileImpl(application)
 
     val data: LiveData<List<Post>> = repository.get()
     val edited = MutableLiveData(empty)
